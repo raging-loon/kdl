914d9e7b1fbf459b4cd3a0af67f6f672d09471dc
raging-loon <85407521+raging-loon@users.noreply.github.com>
2025-10-14 08:53:55 -0400

    reorganization

--- kdlc/analysis/SemanticAnalyzer.cpp
+++
@@ -1,2 +0,0 @@
-#include "SemanticAnalyzer.h"
-#include "RuleIR.h"
---
+++ kdlc/intermediate/Symbol.h
@@ -0,0 +1,15 @@
+#ifndef KDLC_INTERMEDIATE_SYMBOL_H_
+#define KDLC_INTERMEDIATE_SYMBOL_H_
+
+
+namespace kdl
+{
+
+
+
+
+} // kdl
+
+
+
+#endif // KDLC_INTERMEDIATE_SYMBOL_H_
